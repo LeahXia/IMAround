cace4df290af3846dd3e0b05a95b3067eed8fda7
Leah Xia <leahxia5@gmail.com>
2019-07-01 23:45:38 -0700

    Project init

---
+++ IMAround/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMAround
+//
+//  Created by Leah Xia on 2019-07-01.
+//  Copyright © 2019 Leah Xia. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
